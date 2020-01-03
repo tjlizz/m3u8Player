4ed701a2c5f83e5d2130e7c5ffe9d1cbf51d3f2a
zeze.li <lizeze.boy@gmail.com>
2020-01-03 17:13:27 +0800

    修改权限

--- src/index.js
+++ src/index.js
@@ -8,36 +8,36 @@ app.use(cookieParser('zzzz'));
 
 
 
-app.use(function (req, res, next) {
-    if (req.url !== '/') {
-        // console.log(req.signedCookies.bwf)
-        // if (req.get('Referer') !== 'http://localhost:3000/') {
-        //     res.status(status[403]).send('Forbidden')
-        // }
-        // else {
-        //     next()
-        // }
-         next()
-    } else {
-        res.cookie("bwf", uuidv1(), { signed: true });
-        next()
-    }
+// app.use(function (req, res, next) {
+//     if (req.url !== '/') {
+//         // console.log(req.signedCookies.bwf)
+//         // if (req.get('Referer') !== 'http://localhost:3000/') {
+//         //     res.status(status[403]).send('Forbidden')
+//         // }
+//         // else {
+//         //     next()
+//         // }
+//          next()
+//     } else {
+//         res.cookie("bwf", uuidv1(), { signed: true });
+//         next()
+//     }
 
-})
+// })
 
-app.use(function (req, res, next) {
-    if (req.url !== '/') {
-        if (req.signedCookies.bwf) {
-            next()
-        } else {
+// app.use(function (req, res, next) {
+//     if (req.url !== '/') {
+//         if (req.signedCookies.bwf) {
+//             next()
+//         } else {
 
-            res.status(403).send('Forbidden')
-        }
-    } else {
-        next()
-    }
+//             res.status(403).send('Forbidden')
+//         }
+//     } else {
+//         next()
+//     }
 
-});
+// });
 app.use(express.static(path.join(__dirname + '/views'), { maxAge: 1000 * 60 * 60 * 24 }));
 
 app.listen(3000, function () {
