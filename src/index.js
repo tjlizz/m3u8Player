04bb37ec9a77e51e57fbc2ee72310f565091c35c
zeze.li <lizeze.boy@gmail.com>
2020-01-06 17:30:22 +0800

    修改倍速

--- src/index.js
+++ src/index.js
@@ -5,43 +5,39 @@ const uuidv1 = require('uuid/v1')
 const status = require('http-status');
 const cookieParser = require('cookie-parser');
 app.use(cookieParser('zzzz'));
-var cors = require('cors')
-app.use(cors())
+// var cors = require('cors')
+// app.use(cors())
+app.use(function (req, res, next) {
+    // console.log(req.url, 'url')
+    if (req.url !== '/') {
+        // console.log(req.get('Referer'),'Referer')
+        if (req.get('Referer') !== 'http://localhost:3000/') {
+            res.status(403).send('Forbidden2')
+        }
+        else {
+            next()
+        }
+        //next()
+    } else {
+        if (!req.signedCookies.bwf)
+            res.cookie("bwf", uuidv1(), { signed: true });
+        next()
+    }
+})
 
-
-
-// app.use(function (req, res, next) {
-//     if (req.url !== '/') {
-//         // console.log(req.signedCookies.bwf)
-//         // if (req.get('Referer') !== 'http://localhost:3000/') {
-//         //     res.status(status[403]).send('Forbidden')
-//         // }
-//         // else {
-//         //     next()
-//         // }
-//          next()
-//     } else {
-//         res.cookie("bwf", uuidv1(), { signed: true });
-//         next()
-//     }
-
-// })
-
-// app.use(function (req, res, next) {
-//     if (req.url !== '/') {
-//         if (req.signedCookies.bwf) {
-//             next()
-//         } else {
-
-//             res.status(403).send('Forbidden')
-//         }
-//     } else {
-//         next()
-//     }
-
-// });
-app.use(express.static(path.join(__dirname + '/views'), { maxAge: 1000 * 60 * 60 * 24 }));
-
+app.use(function (req, res, next) {
+    console.log(req.url, 'url111')
+    if (req.url !== '/') {
+        if (req.signedCookies.bwf) {
+            next()
+        } else {
+            res.status(401).send('Unauthorized')
+        }
+    } else {
+        next()
+    }
+});
+app.use(express.static(path.join(__dirname + '/views')));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
